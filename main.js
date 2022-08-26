76e3dc2c767aaa56f864db5c4623f4fa88e08f86
kwonhaewon <kwonhaewon@kwonhaewons-MacBook-Pro.local>
2022-08-26 17:42:14 +0900

    CHAPTER19:홈페이지 구현

--- main.js
+++ main.js
@@ -6,46 +6,69 @@ var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
   var pathname = url.parse(_url,true).pathname;
-  var title = queryData.id;
+ 
   if(pathname==="/"){
-    fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
-        var template = `
-        <!doctype html>
-    <html>
+    if(queryData.id===undefined){ 
+        fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
+            var title="Welcome";
+            var description="Hello, Node.js";
+            var template = `
+            <!doctype html>
+        <html>
+        
+        <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+        </head>
+        
+        <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+                <li><a href="/?id=html">HTML</a></li>
+                <li><a href="/?id=css">CSS</a></li>
+                <li><a href="/?id=javascript">JavaScript</a></li>
+            </ol>
+            <h2>${title}</h2>
+            <p>${description}</p>
+        </body>
+        </html>
+            `;
+            response.writeHead(200);
+            response.end(template);
+          });
+    }else{
+        fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
+            var title = queryData.id;
+            var template = `
+            <!doctype html>
+        <html>
+        
+        <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+        </head>
+        
+        <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+                <li><a href="/?id=html">HTML</a></li>
+                <li><a href="/?id=css">CSS</a></li>
+                <li><a href="/?id=javascript">JavaScript</a></li>
+            </ol>
+            <h2>${title}</h2>
+            <p>${description}</p>
+        </body>
+        </html>
+            `;
+            response.writeHead(200);
+            response.end(template);
+          });
+    }
     
-    <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-    </head>
-    
-    <body>
-        <h1><a href="/">WEB</a></h1>
-        <ol>
-            <li><a href="/?id=html">HTML</a></li>
-            <li><a href="/?id=css">CSS</a></li>
-            <li><a href="/?id=javascript">JavaScript</a></li>
-        </ol>
-        <h2>${title}</h2>
-        <p>${description}</p>
-    </body>
-    </html>
-        `;
-        response.writeHead(200);
-        response.end(template);
-      });
   }else{
         response.writeHead(404);
         response.end("NOT FOUND");
   }
 
-  console.log()
-//   if (_url == "/") {
-//     title = "Welcome";
-//   }
-//   if (_url == "/favicon.ico") {
-//     return response.writeHead(404);
-//   }
-  
-  
 });
 app.listen(3000);
