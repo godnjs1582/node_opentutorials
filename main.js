4503c26cc22d42647bd8d8c0b8fcdc296107c6ba
kwonhaewon <kwonhaewon@kwonhaewons-MacBook-Pro.local>
2022-08-26 16:46:46 +0900

    CHAPTER10:URL을 통해서 입력된 값 사용하기

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 var http = require("http");
 var fs = require("fs");
+var url =require("url");//url이라는 모듈의 사용
 
 var app=http.createServer(function(request,response){
-    var url =request.url;
-
-    if(url==="/"){
-        url="/index.html";
+    var _url =request.url;
+    var queryData =url.parse(_url, true).query;
+    console.log(queryData.id);
+    if(_url=="/"){
+      _url = "/index.html";
     }
-    if(url==="/favicon.ico"){
+    if(_url=="/favicon.ico"){
         return response.writeHead(404);
     }
     response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url));
+    response.end(queryData.id);
 });
 app.listen(3000);
