fad4b16c841828d20de1831c19f3fb7cfbf02dff
kwonhaewon <kwonhaewon@kwonhaewons-MacBook-Pro.local>
2022-08-30 17:41:19 +0900

    CHAPTER38:글삭제 기능 완성

--- main.js
+++ main.js
@@ -148,6 +148,20 @@ var app = http.createServer(function (request, response) {
               })
             })
         })
+        
+  }else if(pathname==="/delete_process"){
+      var body ="";
+      request.on("data",function(data){
+        body=body+data;
+      });
+      request.on("end",function(data){
+        var post =qs.parse(body);
+        var id =post.id;
+        fs.unlink(`data/${id}`, function(error){
+          response.writeHead(302,{Location:`/`});
+          response.end();
+        })
+      })
   } else {
     response.writeHead(404);
     response.end("NOT FOUND");
