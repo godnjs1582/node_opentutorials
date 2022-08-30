a5bec3d6590472b0107f1fd31cf57ecab01ff7f0
kwonhaewon <kwonhaewon@kwonhaewons-MacBook-Pro.local>
2022-08-30 15:01:47 +0900

    CHAPTER33:파일생성과 리다이렉션

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require("http");
 var fs = require("fs");
 var url = require("url"); //url이라는 모듈의 사용
+var qs =require("querystring");//querystring이라는 모듈의 사용
 function templateHTML(title, list, body) {
   return `
     <!doctype html>
@@ -71,12 +72,11 @@ var app = http.createServer(function (request, response) {
   } else if (pathname === "/create") {
     fs.readdir("./data", function (error, fileList) {
       var title = "WEB-create";
-      // var description = "Hello, Node.js";
       var list = templateList(fileList);
       var template = templateHTML(
         title,
         list,
-        `<form action="http://localhost:3000/process_create" method="post">
+        `<form action="http://localhost:3000/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p>
                 <textarea name="description" placeholder="description"></textarea>
@@ -89,6 +89,19 @@ var app = http.createServer(function (request, response) {
       response.writeHead(200);
       response.end(template);
     });
+  } else if (pathname === "/create_process") {
+    var body="";
+    request.on("data",function(data){
+        body=body+data;
+    });
+    request.on("end",function(){
+        var post=qs.parse(body);
+        var title=post.title;
+        var description=post.description;
+        console.log(title,description);
+    });
+    response.writeHead(200);
+    response.end("success");
   } else {
     response.writeHead(404);
     response.end("NOT FOUND");
