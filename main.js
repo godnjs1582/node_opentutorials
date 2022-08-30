4c727510c9afd0265cd0ab8f142c012256b4aef2
kwonhaewon <kwonhaewon@kwonhaewons-MacBook-Pro.local>
2022-08-30 15:50:07 +0900

    CHAPTER35:글수정,수정할 정보 전송

--- main.js
+++ main.js
@@ -53,15 +53,10 @@ var app = http.createServer(function (request, response) {
       });
     } else {
       fs.readdir("./data", function (error, fileList) {
-        fs.readFile(
-          `data/${queryData.id}`,
-          "utf8",
-          function (err, description) {
+        fs.readFile(`data/${queryData.id}`,"utf8",function (err, description) {
             var title = queryData.id;
             var list = templateList(fileList);
-            var template = templateHTML(
-              title,
-              list,
+            var template = templateHTML(title,list,
               `<h2>${title}</h2>${description}`,
               `<a href="/create">create</a>
                <a href="/update?id=${title}">update</a>`
@@ -79,7 +74,7 @@ var app = http.createServer(function (request, response) {
       var template = templateHTML(
         title,
         list,
-        `<form action="http://localhost:3000/create_process" method="post">
+        `<form action="/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p>
                 <textarea name="description" placeholder="description"></textarea>
@@ -93,19 +88,44 @@ var app = http.createServer(function (request, response) {
       response.end(template);
     });
   } else if (pathname === "/create_process") {
-    var body="";
-    request.on("data",function(data){
-        body=body+data;
+    var body = "";
+    request.on("data", function (data) {
+      body = body + data;
     });
-    request.on("end",function(){
-        var post=qs.parse(body);
-        var title=post.title;
-        var description=post.description;
-        fs.writeFile(`data/${title}`,description,"utf-8",function(err){
-            response.writeHead(302,{Location:`/?id=${title}`});
-            response.end("success");
+    request.on("end", function () {
+      var post = qs.parse(body);
+      var title = post.title;
+      var description = post.description;
+      fs.writeFile(`data/${title}`, description, "utf-8", function (err) {
+        response.writeHead(302, { Location: `/?id=${title}` });
+        response.end("success");
+      });
+    });
+  } else if (pathname === "/update") {
+    fs.readdir('./data',function(error,fileList){
+        fs.readFile(`data/${queryData.id}`,`utf8`,function(err,description){
+            var title = queryData.id;
+            var list = templateList(fileList);
+            var template = templateHTML(
+              title,
+              list,
+              `<form action="/update_process" method="post">
+                <input type="hidden" name="id" value="${title}">
+                <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+                <p>
+                    <textarea name="description" placeholder="description">${description}</textarea>
+                </p>
+                <p>
+                    <input type="submit">
+                </p>
+            </form>`
+            ,
+            `<a href="/create>create</a><a href="/update?id=${title}">update</a>`
+            );
+            response.writeHead(200);
+            response.end(template);
         })
-    });
+    })
   } else {
     response.writeHead(404);
     response.end("NOT FOUND");
