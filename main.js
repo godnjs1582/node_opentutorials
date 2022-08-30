d99758cd5a879d8906641f05e010dec7dbf1f01f
kwonhaewon <kwonhaewon@kwonhaewons-MacBook-Pro.local>
2022-08-30 16:47:08 +0900

    CHAPTER36:글수정,파일명 변경, 내용저장

--- main.js
+++ main.js
@@ -124,8 +124,25 @@ var app = http.createServer(function (request, response) {
             );
             response.writeHead(200);
             response.end(template);
+        });
+    });
+    }else if(pathname==="/update_process"){
+        var body="";
+        request.on("data",function(data){
+            body=body+data;
+        });
+        request.on("end",function(){
+            var post = qs.parse(body);
+            var id = post.id;
+            var title = post.title;
+            var description = post.description;
+            fs.rename(`data/${id}`, `data/${title}`, function(error){
+              fs.writeFile(`data/${title}`,description,'utf-8',function(error){
+                response.writeHead(302,{Location:`/?id=${title}`});
+                response.end();
+              })
+            })
         })
-    })
   } else {
     response.writeHead(404);
     response.end("NOT FOUND");
